f632b71b41a2e3d078fea13eba048fb2479b7dc9
Martin Lazar-Rudolf <m.lazarrudolf@gmail.com>
2018-08-01 19:00:30 +0200

    741157636499881 Neuanmeldung: implement 201 and message code for creating users

--- Controller/ResourceController.php
+++ Controller/ResourceController.php
@@ -192,8 +192,17 @@ class ResourceController extends Controller
         if (0 === count($violations)) {
             // Validation succeed
             $this->get('event_dispatcher')->dispatch(Events::PRE_CREATE, new DataEvent($resource, $object));
-
-            return $this->getSuccessResponse($resource, $object, 201);
+    
+            if (strpos($resource->getEntityClass(), 'User') !== false) {
+                $data['code'] = 'EMAIL_NOT_CONFIRMED';
+                return new Response(
+                    $data,
+                    201
+                );
+            } else {
+                return $this->getSuccessResponse($resource, $object, 201);
+            }
+            
         }
 
         return $this->getErrorResponse($violations);
